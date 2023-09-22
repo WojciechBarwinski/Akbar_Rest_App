bb2fe9ef2ee28d0f1746b6f6139a5ec985705785
Wojtek B. <wojciech.barwinski@gmail.com>
2023-09-22 09:19:55 +0200

    initial commit

---
+++ src/main/java/com/wojciech/barwinski/akbarrestapp/AkbarRestAppApplication.java
@@ -0,0 +1,15 @@
+package com.wojciech.barwinski.akbarrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbarRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbarRestAppApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/wojciech/barwinski/akbarrestapp/AkbarRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojciech.barwinski.akbarrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkbarRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
